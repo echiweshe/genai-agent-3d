b370f4dd4b2ead8679d079d665a1228f5cb1e18b
ErnestC <ernestchiweshe@gmail.com>
2025-04-19 12:47:41 +0200

    Now that we've made all the necessary changes to fix the frontend testing environment, let's list what we've done and what you should try next: Fixed the setup.js file: We added a check for the window object and created a global window object if it doesn't exist, which is common in Node.js environments. Created a Jest configuration file (jest.config.js): Specified the jsdom environment, set up the proper test paths, and configured coverage and transformations. Created mock files for CSS and images: Added styleMock.js and fileMock.js to handle non-JS imports. Added a WebSocket service test: Created a test file to validate the WebSocket service functionality. Updated the run_tests.js script: Modified it to use our Jest configuration file. To test these changes, try running the WebSocket tests with the following command: bashcd C:\ZB_Share\Labs\src\CluadeMCP\genai-agent-3d\genai_agent_project\web\frontend node run_tests.js --unit-only

---
+++ genai_agent_project/web/frontend/babel.config.js
@@ -0,0 +1,13 @@
+/**
+ * Babel configuration for Jest tests
+ */
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime'
+  ]
+};
